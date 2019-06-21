01b0cfc9875caf8f710c4d23cc41d23f249ceafc
Anton Nesterov <arch.nesterov@gmail.com>
2019-06-22 01:46:07 +0300

    [deps] no dependencies

--- index.js
+++ index.js
@@ -1,69 +1,66 @@
-
-
-const observe = require('on-change')
-
 module.exports.getInitial = function(name) {
-  if (typeof document !== 'undefined')
-    return (document.__GLOBAL_SHARED_STATE || {}) [name]
+  if (typeof document !== "undefined")
+    return (document.__GLOBAL_SHARED_STATE || {})[name];
 };
 
-module.exports.createStore = function ({ name, state, actions }){
-  
-  
-  let _state = state
-  
+module.exports.createStore = function({ name, state, actions }) {
+  let _state = state;
+
   const saveCopy = () => {
-    if (typeof document !== 'undefined') {
-      document.__GLOBAL_SHARED_STATE = document.__GLOBAL_SHARED_STATE || {}
-      document.__GLOBAL_SHARED_STATE [name] =_state
+    if (typeof document !== "undefined") {
+      document.__GLOBAL_SHARED_STATE = document.__GLOBAL_SHARED_STATE || {};
+      document.__GLOBAL_SHARED_STATE[name] = _state;
     }
-  }
+  };
 
-  const subscribtions = {}
+  const subscribtions = {};
   const subscribe = (prop, cb) => {
-    subscribtions[prop] = subscribtions[prop] || []
-    if (!subscribtions[prop].includes(cb))
-      subscribtions[prop].push(cb);
-  }
+    subscribtions[prop] = subscribtions[prop] || [];
+    if (!subscribtions[prop].includes(cb)) subscribtions[prop].push(cb);
+  };
 
-  observe(_state, (path)=>{
-    (subscribtions[path] || []).map((cb) => {
-      cb()
-    })
-  })
-
-  const install = (component) => {
-    (component.shared || []).map(e => {
+  const install = component => {
+    (component.shared || []).forEach(e => {
       Object.defineProperty(component, e, {
-        get(){
-          return _state[e]
+        get() {
+          return _state[e];
         }
-      })
+      });
       subscribe(e, () => {
-        component.update()
-      })
-    })
-  }
+        component.update();
+      });
+    });
+  };
 
-  const dispatch = function (action){
-    const ctx = {
-      state: _state,
-      actions,
-    };
-    const result = actions[action].apply(ctx, Array.from(arguments).slice(1))
-    if (result && typeof result === 'object' && result.then) {
-      return result.then(saveCopy)
+  const dispatch = function(action) {
+    const ctx = {};
+    Object.keys(state).forEach(element => {
+      Object.defineProperty(ctx, element, {
+        get: function() {
+          return state[element];
+        },
+        set: function(v) {
+          state[element] = v;
+          (subscribtions[element] || []).map(function(cb) {
+            return cb();
+          });
+        }
+      });
+    });
+
+    const result = actions[action].apply(ctx, Array.from(arguments).slice(1));
+    if (result && typeof result === "object" && result.then) {
+      return result.then(saveCopy);
     } else {
-      saveCopy()
-      return result
+      saveCopy();
+      return result;
     }
-  }
+  };
 
   return {
     state: _state,
     dispatch,
     subscribe,
-    install,
-  }
-
-}
+    install
+  };
+};
